10ce7b72c30978d906148fe0edc4840942b9be3f
Alex-de-bug <113011463+Alex-de-bug@users.noreply.github.com>
2023-09-19 03:54:42 +0300

    add logic for card page

---
+++ app/utils/discountForCardsData.ts
@@ -0,0 +1,20 @@
+const columns = [
+    { name: "ID", uid: "id",  sortable: true},
+    { name: "Название", uid: "name",  sortable: true},
+    { name: "Значение", uid: "value",  sortable: true},
+    { name: "Пол", uid: "sex",  sortable: true },
+    { name: "Возраст", uid: "age"},
+    { name: "Срок годности", uid: "time_active"},
+    { name: "Статус", uid: "status", sortable: true  },
+    { name: "ЕКЖ", uid: "izk", sortable: true  },
+    { name: "Номер карты", uid: "card_data"  },
+    { name: "Действия", uid: "actions" },
+];
+
+const statusOptions = [
+    {name: "Активна", uid: "active"},
+    {name: "Удалена", uid: "delete"},
+    {name: "Приостановлена", uid: "pause"},
+];
+
+export {columns,  statusOptions};
